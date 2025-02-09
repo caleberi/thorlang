e5a1d5246e738ade31168b46942e2c3cc02b7ea0
caleberi <caleberioluwa@gmail.com>
2025-02-09 08:01:51 +0100

    chore: move grammar representation to header

--- include/compiler.h
+++ include/compiler.h
@@ -15,4 +15,11 @@ bool compile(const char *source, Chunk *chunk);
 void boostrap_keyword();
 void cleanup_keyword();
 
+static void binary();
+static void unary();
+static void grouping();
+static void expression();
+static void number();
+static ParseRule *get_rule(TokenType);
+
 #endif // _THOR_COMPILE_H_
