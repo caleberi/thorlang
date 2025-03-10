de28ccb1437e37f4037aa2000991b801eb7623c4
caleberi <caleberioluwa@gmail.com>
2025-03-10 18:05:36 +0100

    feat: -  use flexible array for string object - add obkect string support

--- include/object.c
+++ include/object.c
@@ -8,10 +8,17 @@
 #define ALLOCATE_OBJ(type, objectType) \
     (type *)allocate_object(sizeof(type), objectType)
 
-static Obj *allocate_object(size_t size, ObjType type)
+#define ALLOCATE_OBJ_PLUS_CHARS(type, objectType, length) \
+    (type *)allocate_object((sizeof(type) + (length + 1)), objectType)
+
+static Obj *
+allocate_object(size_t size, ObjType type)
 {
     Obj *object = (Obj *)reallocate(NULL, 0, size);
     object->type = type;
+
+    object->next = vm.objects;
+    vm.objects = object;
     return object;
 }
 
@@ -23,11 +30,19 @@ ObjString *copy_string(const char *chars, int length)
     return allocate_string(heap_chars, length);
 }
 
+ObjString *take_string(char *chars, int length)
+{
+    return allocate_string(chars, length);
+}
+
 static ObjString *allocate_string(char *chars, int length)
 {
-    ObjString *string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
+    ObjString *string = ALLOCATE_OBJ_PLUS_CHARS(ObjString, OBJ_STRING, length);
+    if (string == NULL)
+        return NULL;
     string->length = length;
-    string->chars = chars;
+    memcpy(string->chars, chars, length);
+    string->chars[length] = '\0';
     return string;
 }
 
