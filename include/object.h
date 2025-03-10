832448ece441c6976a7a01a7705f0e23f9536ad6
caleberi <caleberioluwa@gmail.com>
2025-03-10 18:06:18 +0100

    feat: add object string tracking

--- include/object.h
+++ include/object.h
@@ -4,6 +4,7 @@
 #include "common.h"
 #include "value.h"
 
+#define AS_OBJ(value) ((value).pas.obj)
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 #define IS_STRING(value) is_obj_type(value, OBJ_STRING)
 #define AS_STRING(value) ((ObjString *)AS_OBJ(value))
@@ -17,16 +18,18 @@ typedef enum ObjType
 struct Obj
 {
     ObjType type;
+    struct Obj *next;
 };
 
 struct ObjString
 {
     Obj obj;
     int length;
-    char *chars;
+    char chars[];
 };
 
 ObjString *copy_string(const char *chars, int length);
+ObjString *take_string(char *chars, int length);
 void print_object(Value value);
 
 static inline bool is_obj_type(Value value, ObjType type)
